23820d4c1c6f16d56c2b640af92c6b569e99c7d9
FrozenPyrozen <kiril.kohan@gmail.com>
2019-08-06 06:15:20 -0400

    Added basic server on http code

---
+++ 1-node-farm/starter/index.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const http = require('http');
+
+/**
+ * Files
+ *
+ */
+// Blocking, synchronus way
+// const fileInput = fs.readFileSync('./txt/input.txt','utf-8');
+// console.log(fileInput);
+
+// const textOut = `${fileInput} I love this avocado!\nCreated on ${Date.now().toLocaleString()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+
+// Nonblocking, asynchronus way
+// const fileInput = fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   const fileInput = fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     const fileInput = fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//       console.log(data3);
+
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//         if (err) console.log(err);
+//         console.log('Your file has ben written:');
+//         fs.readFile(`./txt/final.txt`, 'utf-8', (err, data4) => {
+//           console.log(data4);
+//         });
+//       });
+//     });
+//   });
+// });
+// console.log('Will read file!');
+
+////////////////////////////////////////////////////////////////////
+// SERVER
+const server = http.createServer((req, res) => {
+  res.end('Hello from the server!');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000!');
+});
