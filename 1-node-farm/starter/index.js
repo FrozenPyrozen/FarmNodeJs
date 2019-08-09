4b5ca105604d3ff83a3613bf4b617a9c076899c7
FrozenPyrozen <kiril.kohan@gmail.com>
2019-08-08 20:25:59 -0400

    Separated read data

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -34,6 +34,9 @@ const url = require('url');
 
 ////////////////////////////////////////////////////////////////////
 // SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
@@ -41,6 +44,9 @@ const server = http.createServer((req, res) => {
     res.end('This is the overview!');
   } else if (pathName === '/product') {
     res.end('This is the product!');
+  } else if (pathName === '/api') {
+    res.writeHead(200, { 'Content-type': 'text/json' });
+    res.end(data);
   } else {
     res.writeHead(404, { 'Content-type': 'text/html' });
     res.end('<h1>Page not found 404!</h1>');
@@ -51,4 +57,4 @@ server.listen(8000, '127.0.0.1', () => {
   console.log('Listening to requests on port 8000!');
 });
 
-// TO-DO: Start watch video 9
+// TO-DO: Start watch video 11
