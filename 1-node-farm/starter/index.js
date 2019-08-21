598c5a0ebdc56bb3dd735f504ab58a5f1c9c48e9
FrozenPyrozen <kiril.kohan@gmail.com>
2019-08-20 22:18:17 -0400

    Added slugify and nodemon dependencies

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,7 +1,7 @@
-const fs = require("fs");
-const http = require("http");
-const url = require("url");
-const replaceTemplate = require("./modules/replaceTemplate");
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+const replaceTemplate = require('./modules/replaceTemplate');
 
 /**
  * Files
@@ -36,54 +36,54 @@ const replaceTemplate = require("./modules/replaceTemplate");
 ////////////////////////////////////////////////////////////////////
 // SERVER
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
-  "utf-8"
+  'utf-8'
 );
 const tempProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
-  "utf-8"
+  'utf-8'
 );
 const tempCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
-  "utf-8"
+  'utf-8'
 );
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
   const { query, pathname } = url.parse(pathName, true);
   // Overview page
-  if (pathname === "/" || pathname === "/overview") {
-    res.writeHead(200, { "Content-type": "text/html" });
-    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join("");
-    const output = tempOverview.replace("%PRODUCT_CARDS%", cardsHtml);
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace('%PRODUCT_CARDS%', cardsHtml);
 
     res.end(output);
 
     // Product page
-  } else if (pathname === "/product") {
-    res.writeHead(200, { "Content-type": "text/html" });
+  } else if (pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
     const product = dataObj[query.id];
     const output = replaceTemplate(tempProduct, product);
     res.end(output);
 
     // API
-  } else if (pathname === "/api") {
-    res.writeHead(200, { "Content-type": "application/json" });
+  } else if (pathname === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' });
     res.end(data);
 
     // Not found
   } else {
-    res.writeHead(404, { "Content-type": "text/html" });
-    res.end("<h1>Page not found 404!</h1>");
+    res.writeHead(404, { 'Content-type': 'text/html' });
+    res.end('<h1>Page not found 404!</h1>');
   }
 });
 
-server.listen(8000, "127.0.0.1", () => {
-  console.log("Listening to requests on port 8000!");
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000!');
 });
 
-// TO-DO: Start watch video 14
+// TO-DO: Start watch video 16
