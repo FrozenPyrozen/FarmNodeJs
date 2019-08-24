0eb710ad3ca9d6284da779e81a3858fc9cee795e
FrozenPyrozen <kiril.kohan@gmail.com>
2019-08-24 08:33:57 -0400

    project initialized

---
+++ 4-natours/starter/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side!', app: 'Natours' })
+    .send();
+});
+
+app.post('/', (req, res) => {
+  res.send('You can post!');
+});
