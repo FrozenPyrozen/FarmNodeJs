e0f75fb6c2f15b28e16f36e09e92fc99de0a1e23
FrozenPyrozen <kiril.kohan@gmail.com>
2019-08-30 11:13:59 -0400

    feat ( rich model ) : tourModel

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -50,7 +50,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: 'Invalid data sent',
+      message: err,
     });
   }
 };
@@ -84,6 +84,6 @@ exports.deleteTour = async (req, res) => {
       data: null,
     });
   } catch (err) {
-    res.status(400).json({ status: 'fail', message: err });
+    res.status(404).json({ status: 'fail', message: err });
   }
 };
